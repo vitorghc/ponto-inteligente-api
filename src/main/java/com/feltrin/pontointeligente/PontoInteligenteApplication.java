1dd28028b338fcb337eb793c06667e0ef0026c03
Vitor Feltrin <vitor.feltrin@sensedia.com>
2019-01-10 08:03:19 -0200

    Commit inicial

---
+++ src/main/java/com/feltrin/pontointeligente/PontoInteligenteApplication.java
@@ -0,0 +1,14 @@
+package com.feltrin.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+}
+
